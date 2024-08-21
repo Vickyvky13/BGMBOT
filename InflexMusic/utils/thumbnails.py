23d5df62724452dcfcd98de50c0637b21256f632
FALLEN <134856609+BtwFallen@users.noreply.github.com>
2024-08-21 13:43:56 +0530

    Update thumbnails.py

--- InflexMusic/utils/thumbnails.py
+++ InflexMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 from InflexMusic import app
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from config import YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
