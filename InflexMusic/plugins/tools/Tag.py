97e1eedf57239b3d208f60a10a62ca8cf3fa5c04
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-09-30 08:34:39 +0530

    Update Tag.py

--- InflexMusic/plugins/tools/Tag.py
+++ InflexMusic/plugins/tools/Tag.py
@@ -46,7 +46,7 @@ async def mention_all(client: Client, message: Message):
         if chat_id not in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"<a href='tg://user?id={usr.user.id}'>{usr.user.first_name}</a>, "
+        usrtxt += f'<a href="tg://user?id={usr.user.id}">{usr.user.first_name}</a>, '
 
         # Send message in chunks of 5 users
         if usrnum == 5:
