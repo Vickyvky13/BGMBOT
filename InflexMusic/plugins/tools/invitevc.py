81304a14bf50cc3965eea41306a4475487c771f1
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-10-22 12:46:48 +0530

    Update invitevc.py

--- InflexMusic/plugins/tools/invitevc.py
+++ InflexMusic/plugins/tools/invitevc.py
@@ -1,17 +1,16 @@
 import random
 from pyrogram import filters
 from pyrogram.enums import ParseMode
-from pyrogram.types import VideoChatParticipantsInvited
 from InflexMusic import app
 import config
 
 # Define the filter for video chat members being invited
 @app.on_message(filters.video_chat_members_invited)
 async def video_chat_members_invited_handler(client, message):
-    # Check if the message contains VideoChatParticipantsInvited
+    # Check if the message contains video chat participants invited info
     if message.video_chat_participants_invited:
         invited_participants = message.video_chat_participants_invited.users
-        # If the bot itself is mentioned in the invited participants
+        # Check if the bot itself is mentioned in the invited participants
         for user in invited_participants:
             if user.is_self:
                 await message.reply_text(
